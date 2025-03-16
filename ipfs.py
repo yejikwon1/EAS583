68b573d5f4bddab15ca59f03bd754ebece20bcbd
Yeji Kwon <yejikwon@seas.upenn.edu>
2025-03-16 19:12:22 -0400

    v1

---
+++ ipfs.py
@@ -0,0 +1,56 @@
+import requests
+import json
+
+PINATA_API_KEY="3294fa2ac3a8b5454d5c"
+PINATA_SECRET_API_KEY="6fce643db98dd2c09352b9eef212e48ebee7575cc69823be978e24a57bbf9cd6"
+PINATA_PIN_JSON_URL="https://api.pinata.cloud/pinning/pinJSONToIPFS"
+
+
+IPFS_GATEWAY= "https://gateway.pinata.cloud/ipfs"
+
+def pin_to_ipfs(data):
+
+	headers={
+		"Content-Type":"application/json",
+		"pinata_api_key":PINATA_API_KEY,
+		"pinata_secret_api_key":PINATA_SECRET_API_KEY,	
+	}
+
+	json_data={
+		"pinataContent":data,
+		"pinataMetadata":{"name":"my_json_data"}
+	}
+
+	response=requests.post(PINATA_PIN_JSON_URL,headers=headers,json=json_data)
+
+	if response.status_code==200:
+		cid=response.json()["IpfsHash"]
+		return cid
+
+	else:
+		return None
+
+	#assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
+	#YOUR CODE HERE
+
+
+
+def get_from_ipfs(cid,content_type="json"):
+	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
+	#YOUR CODE HERE	
+
+	url=f"{IPFS_GATEWAY}{cid}"
+	response=requests.get(url)
+
+	if response.status_code==200:
+		try:
+			if content_type=="json":
+				data=response.json()
+				assert isinstance(data,dict), "get_from_ipfs should return a dict"
+			else:
+				data=response.text
+		except requests.exceptions.JSONDecodeError:
+			raise ValueError
+		return data
+	else:
+		raise Exception(f"failed")
