442218b9184168b9f8d3731376693021e2fe7e44
Yeji Kwon <yejikwon@seas.upenn.edu>
2025-03-30 20:31:54 -0400

    commit2

--- gen_keys.py
+++ gen_keys.py
@@ -3,7 +3,7 @@ from eth_account.messages import encode_defunct
 import eth_account
 import os
 
-def sign_message(challenge, filename="secret_key.txt"):
+def get_keys (challenge, filename="secret_key.txt"):
     """
     challenge - byte string
     filename - filename of the file that contains your account secret key
@@ -16,29 +16,27 @@ def sign_message(challenge, filename="secret_key.txt"):
         key = f.readlines()
     assert(len(key) > 0), "Your account secret_key.txt is empty"
 
-    w3 = Web3()
-    message = encode_defunct(challenge)
+    private_key=key[0].strip()
+    acct=eth_account.Account.from_key(private_key)
+    eth_addr=acct.address
+    message = encode_defunct(text=challenge.hex())
 
+    #w3 = Web3()
+    
     # TODO recover your account information for your private key and sign the given challenge
     # Use the code from the signatures assignment to sign the given challenge
     
-
-    private_key=key[0].strip()
-    acct=eth_account.Account.from_key(private_key)
-
     signed_message=acct.sign_message(message)
-
-    eth_addr=acct.address
-
+    recovered=eth_account.Account.recover_message(message,signed_message.signature)
 
     assert eth_account.Account.recover_message(message,signature=signed_message.signature.hex()) == eth_addr, f"Failed to sign message properly"
 
     #return signed_message, account associated with the private key
-    return signed_message, eth_addr
-
+  #  return signed_message, eth_addr
+    return {"address": eth_addr, "signature":signed_message.signature.hex()}
 
 if __name__ == "__main__":
     for i in range(4):
         challenge = os.urandom(64)
-        sig, addr= sign_message(challenge=challenge)
-        print( addr )
+        result=get_keys(challenge)
+        print(result )
