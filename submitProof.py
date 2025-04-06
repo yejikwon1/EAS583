6cb2ea623f9bcb3403fa72985e3beee30ce41bc7
Yeji Kwon <yejikwon@seas.upenn.edu>
2025-04-06 11:21:22 -0400

    commit3

--- submitProof.py
+++ submitProof.py
@@ -173,7 +173,7 @@ def send_signed_msg(proof, random_leaf):
     })
 
     signed_tx=w3.eth.account.sign_transaction(tx,private_key=acct.key)
-    tx_hash=w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    tx_hash=w3.eth.send_raw_transaction(signed_tx.raw_Transaction)
 
     return tx_hash.hex()
 
