15d1e6ad16a6b1ee2bdbff2002a1756159b621a5
Yeji Kwon <yejikwon@seas.upenn.edu>
2025-04-06 18:04:57 -0400

    commit9

--- submitProof.py
+++ submitProof.py
@@ -120,11 +120,10 @@ def prove_merkle(merkle_tree, random_indx):
     """
     proof = []
     # TODO YOUR CODE HERE
-    index=random_indx
+
     for level in merkle_tree[:-1]:
-        pair_index=index^1
-        if pair_index<len(level):
-            proof.append(level[pair_index])
+        if (index^1)<len(level):
+            proof.append(level[index^1])
         index //=2
     return proof
 
