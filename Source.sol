f932fe76abfe31c12aac8ec9d558b56d3262484d
Yeji Kwon <yejikwon@seas.upenn.edu>
2025-04-09 21:25:21 -0400

    commit7

--- Source.sol
+++ Source.sol
@@ -24,8 +24,6 @@ contract Source is AccessControl {
 	function deposit(address _token, address _recipient, uint256 _amount ) public {
 		//YOUR CODE HERE
 		require(approved[_token],"token not approved");
-		//require(_amount>0,"amount>0");
-
 		bool success=ERC20(_token).transferFrom(msg.sender,address(this),_amount);
 		require(success,"failed");
 		emit Deposit(_token,_recipient,_amount);
@@ -35,8 +33,7 @@ contract Source is AccessControl {
 	function withdraw(address _token, address _recipient, uint256 _amount ) onlyRole(WARDEN_ROLE) public {
 		//YOUR CODE HERE
 		require(_amount>0,"amount>0");
-
-		bool success=ERC20(_token).transfer(_recipient,_amount);
+		bool success=ERC20(_token).transferFrom(_recipient,_amount);
 		require(success,"failed");
 		emit Withdrawal(_token,_recipient,_amount);
 	
@@ -45,7 +42,7 @@ contract Source is AccessControl {
 	function registerToken(address _token) onlyRole(ADMIN_ROLE) public {
 		//YOUR CODE HERE
 		require(!approved[_token],"token registered");
-		approved[_token]=true;
+		//approved[_token]=true;
 		emit Registration(_token);
 
 
